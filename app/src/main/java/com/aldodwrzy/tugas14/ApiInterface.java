19814cc817e2493ae9d05f990f10e63192c53e30
aldodwrzy <aldodwrzy@gmail.com>
2022-02-17 13:56:05 +0700

    First Commit

---
+++ app/src/main/java/com/aldodwrzy/tugas14/ApiInterface.java
@@ -0,0 +1,56 @@
+package com.aldodwrzy.tugas14;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+/**
+ * Created by haerul on 15/03/18.
+ */
+
+public interface ApiInterface {
+
+    @POST("get_pets.php")
+    Call<List<Mahasiswa>> getPets();
+
+    @FormUrlEncoded
+    @POST("add_pet.php")
+    Call<Mahasiswa> insertPet(
+            @Field("key") String key,
+            @Field("name") String name,
+            @Field("species") String species,
+            @Field("breed") String breed,
+            @Field("gender") int gender,
+            @Field("birth") String birth,
+            @Field("picture") String picture);
+
+    @FormUrlEncoded
+    @POST("update_pet.php")
+    Call<Mahasiswa> updatePet(
+            @Field("key") String key,
+            @Field("id") int id,
+            @Field("name") String name,
+            @Field("species") String species,
+            @Field("breed") String breed,
+            @Field("gender") int gender,
+            @Field("birth") String birth,
+            @Field("picture") String picture);
+
+    @FormUrlEncoded
+    @POST("delete_pet.php")
+    Call<Mahasiswa> deletePet(
+            @Field("key") String key,
+            @Field("id") int id,
+            @Field("picture") String picture);
+
+    @FormUrlEncoded
+    @POST("update_love.php")
+    Call<Mahasiswa> updateLove(
+            @Field("key") String key,
+            @Field("id") int id,
+            @Field("love") boolean love);
+
+}
